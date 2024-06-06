f0d3d1d5663ef7d72c18073e83f87cdb52e5f823
jessmdeleon <161788302+jessmdeleon@users.noreply.github.com>
2024-06-05 20:13:36 -0400

    Update Books.jsx
    Brandon S

--- Unit3.BookBuddy.Starter/src/components/Books.jsx
+++ Unit3.BookBuddy.Starter/src/components/Books.jsx
@@ -1,26 +1,40 @@
 /* TODO - add your code to create a functional React component that displays all of the available books in the library's catalog. Fetch the book data from the provided API. Users should be able to click on an individual book to navigate to the SingleBook component and view its details. */
+import { fetchAllBooks } from "../api";
+import { useState, useEffect } from "react";
+import SingleBook from "./SingleBook";
 
-import { fetchAllBooks } from "../api"
-import { useState, useEffect } from "react"
-import BookCard from "./BookCard"
+export default function Books() {
+  const [books, setBooks] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
+  useEffect(() => {
+    const fetchBooks = async () => {
+      try {
+        const response = await fetchAllBooks();
+        setBooks(response);
+      } catch (error) {
+        setError("Failed to fetch books");
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchBooks();
+  }, []);
 
-const Books = () => {
-    const [books, setBooks] = useState([])
-    useEffect (() => {
-        async function getBooks() {
-            const newBooks = await fetchAllBooks()
-            setBooks(newBooks)
-        }
-        getBooks()
-    }, [])
-    return <div>
-        {
-            books.map((book, index) => {
-                return <BookCard key={index} book = {book}/>
-            })
-        }
+  return (
+    <div>
+      {loading && <p>Loading...</p>}
+      {error && <p>{error}</p>}
+      {books && (
+        <div className="books">
+          {/*<div className="book-list">*/}
+          {books.map((book) => (
+            <SingleBook key={book.id} book={book} />
+          ))}
+          {/*</div>*/}
+        </div>
+      )}
     </div>
+  );
 }
-
-export default Books
