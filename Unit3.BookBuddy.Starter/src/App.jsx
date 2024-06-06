cf30b1e95f36ad99c74af64cba5df8ccd3d466c3
jessmdeleon <161788302+jessmdeleon@users.noreply.github.com>
2024-06-05 20:21:20 -0400

    Update App.jsx
    update changes

--- Unit3.BookBuddy.Starter/src/App.jsx
+++ Unit3.BookBuddy.Starter/src/App.jsx
@@ -1,22 +1,44 @@
-import { useState } from 'react'
-import bookLogo from './assets/books.png'
-import Books from './components/Books'
-import Login from './components/Login'
+import bookLogo from "./assets/books.png";
+import Books from "./components/Books";
+import Account from "./components/Account";
+import Home from "./components/Home";
+import Navigations from "./components/Navigations";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import Login from "./components/Login";
+import Register from "./components/Register";
+// import SingleBook from "./components/SingleBook";
+// import { useState } from "react";
 
 function App() {
-  const [token, setToken] = useState(null)
+  // const [token, setToken] = useState(null);
 
   return (
-    <>
-    <div className="App">
-      <header className='App-header'>
-        <Login />
-      </header>
-    </div>
-      <h1><img id='logo-image' src={bookLogo}/>Library App</h1>
-      <Books />
-    </>
+    <Router>
+      <div>
+        <header>
+          <img src={bookLogo} alt="book logo" className="logo" />
+          <h1>Book Buddy</h1>
+        </header>
+
+        <Navigations />
+        <Routes>
+          <Route path="/Home" element={<Home to="/Home" />} />
+          <Route path="/books" element={<Books />} />
+          <Route path="/account" element={<Account />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/" element={<Navigate to="/Home" />} />
+          {/* <Route path="/books/:id" element={<SingleBook />} /> */}
+        </Routes>
+      </div>
+
+    </Router>
   );
 }
 
-export default App
+export default App;
